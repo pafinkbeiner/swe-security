221d89119bdcd055977c314b8d70d365a54b2aaa
pafinkbeiner <paul-d-f@web.de>
2020-11-08 16:19:25 +0100

    started middleware

---
+++ Code/db/src/Helper/Auth.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+export function authMiddleware(req: Request, res: Response ,next: NextFunction){
+    //Check Boolean 
+    const check: boolean = false;
+
+    if(){
+
+    }
+}
