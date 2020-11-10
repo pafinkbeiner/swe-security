7f1cdf752cbea738211e9cce19527ef8d89e813c
pafinkbeiner <paul-d-f@web.de>
2020-11-10 14:51:32 +0100

    Transaction process

--- Code/bank/src/routes/index.ts
+++ Code/bank/src/routes/index.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import { JsonDB } from "node-json-db";
 import { nextTick } from "process";
 import { errorMonitor } from "stream";
 import { authMiddleware } from "../Helper/Auth";
@@ -25,21 +26,52 @@ router.get("/getBalance/:IBAN", (req, res, next) => {
 })
 
 router.post("/new", (req, res, next) => {
-
-  if(req.body.IBAN == undefined || req.body.firstname == undefined || req.body.lastname == undefined || req.body.balance == undefined){
-    next();
-  }
+  if(req.body.IBAN == undefined) res.json(errorStatus.msg = "IBAN was not provided");
+  if(req.body.firstname == undefined) res.json(errorStatus.msg = "firstname was not provided");
+  if(req.body.lastname == undefined) res.json(errorStatus.msg = "lastname was not provided");
+  if(req.body.balance == undefined) res.json(errorStatus.msg = "balance was not provided");
 
   const account: Account = {
     IBAN: req.body.IBAN,
     firstname: req.body.firstname,
     lastname: req.body.lastname,
-    balance: req.body.balance
+    balance: Number.parseInt(req.body.balance)
   }
 
   DatabaseHandler.getDbInstance().set(account.IBAN, account);
 
   res.json(successStatus);
+
+});
+
+router.get("/transaction/:IBANFROM/:IBANTO/:amount", (req, res, next) => {
+
+  if(req.params.IBANFROM == undefined) res.json(errorStatus.msg = "IBANFROM was not provided");
+  if(req.params.IBANTO == undefined) res.json(errorStatus.msg = "IBANTO was not provided");
+  if(req.params.amount == undefined) res.json(errorStatus.msg = "amount was not provided");
+
+  let accountFROM: Account = DatabaseHandler.getDbInstance().get(req.params.IBANFROM);
+  let accountTO: Account = DatabaseHandler.getDbInstance().get(req.params.IBANTO);
+
+  if(accountFROM == undefined) res.json(errorStatus.msg = `No Account was found with IBAN: ${req.params.IBANFROM} was not provided`);
+  if(accountTO == undefined) res.json(errorStatus.msg = `No Account was found with IBAN: ${req.params.IBANTO} was not provided`);
+
+  const amountString: string = req.params.amount;
+
+  const amount: number = Number.parseInt(amountString);
+
+  accountFROM.balance = accountFROM.balance - amount;
+  accountTO.balance = accountTO.balance + amount;
+
+  DatabaseHandler.getDbInstance().update(req.params.IBANTO, accountTO);
+  DatabaseHandler.getDbInstance().update(req.params.IBANFROM, accountFROM);
+
+  res.json(successStatus.msg="Transaction was completed successfully");
+});
+
+router.get("/all", (req, res, next) => {
+
+  res.json(DatabaseHandler.getDbInstance().getAll());
 
 });
 
