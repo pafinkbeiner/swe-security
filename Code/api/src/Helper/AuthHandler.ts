1a32beca438852e81a2fecd4f2012ef0a31827c0
pafinkbeiner <paul-d-f@web.de>
2020-11-25 20:21:56 +0100

    added authMiddleware

--- Code/api/src/Helper/AuthHandler.ts
+++ Code/api/src/Helper/AuthHandler.ts
@@ -3,16 +3,39 @@ import bcrypt from "bcrypt"
 import { DatabaseHandler } from "./Database";
 import {User} from "../models/User"
 import { Role } from "../models/Role";
-import { resolve } from "path";
 import { LogHandler } from "./Log";
+import { NextFunction, Request, Response } from "express";
 
+export function authMiddleware(req: Request, res: Response ,next: NextFunction){
+
+    let bearer: any = req.headers["authorization"];
+
+    if(bearer != undefined){
+
+        const token = bearer.split(" ")[1];
+
+        jwt.verify(token, "secret", (err:any, authData: any) => {
+
+            if(err){
+                res.status(400).json({error: "Verification was not successfull"});
+            }else{
+                next();
+            }
+
+        });
+
+    }else{
+        res.status(400).json({error: "Bearer token was not provided"});
+    }
+
+}
 
 export async function login(username: string, password:string){
 
     LogHandler.getLogInstance().log(`info that user with username: ${username} tried to login`);
 
     let user;
-    
+
     try{
         user = await (await DatabaseHandler.getDbInstance().get(username)).data;
     }catch{
@@ -22,7 +45,7 @@ export async function login(username: string, password:string){
     if(user != undefined){
 
         if(bcrypt.compareSync(password, user.password)){
-            return {key: jwt.sign(user,"secret")};
+            return {key: jwt.sign(user, "secret" )};
         }
 
     }else{
--- Code/api/src/routes/items.ts
+++ Code/api/src/routes/items.ts
@@ -3,9 +3,10 @@ import * as express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  //Print out all items
-  next();
+router.get("/", async(req, res, next) => {
+  
+  
+
 });
 
 export default router;
