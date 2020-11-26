545d99d49ef5b8082841760c292df4bfa9abd6d0
pafinkbeiner <paul-d-f@web.de>
2020-11-26 14:18:00 +0100

    Started with Admin

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -5,14 +5,16 @@ import Register from './components/Register/Register';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { CookiesProvider } from "react-cookie"
 import Items from './components/Items/Items';
+import Admin from "./components/Admin/Admin";
 
 function App() {
   return (
     <CookiesProvider>
       <Router>
         <Switch>
+          <Route path="/admin" component={Admin}/>
           <Route path="/items" component={Items} />
-          <Route path="/Login" component={Login}/>
+          <Route path="/login" component={Login}/>
           <Route path="/" component={Register}/>
         </Switch>
       </Router>
