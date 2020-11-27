84b6bed157f8e54ea3c3eaeaa5fb17107bd95f43
pafinkbeiner <paul-d-f@web.de>
2020-11-27 16:07:25 +0100

    sad

--- Code/frontend/src/components/Register/Register.jsx
+++ Code/frontend/src/components/Register/Register.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import {Link, Redirect} from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Register.scss";
-import axios from 'axios';
+import axios from "axios";
 
 export default class Login extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       username: "",
       password: "",
       mail: "",
-      redirect: null
+      redirect: null,
     };
     this.onSubmit = this.onSubmit.bind(this);
   }
@@ -21,53 +20,85 @@ export default class Login extends Component {
     return this.state.username.length === 0 || this.state.password.length === 0;
   }
 
-  onSubmit = (e) =>{
-
+  onSubmit = (e) => {
     e.preventDefault();
 
-    axios.post('http://localhost:5001/register',{
+    axios
+      .post("http://localhost:5001/register", {
         username: this.state.username,
         password: this.state.password,
-        mail: this.state.mail
-    }).then(auth => {
-
-        if(auth){
-            localStorage.setItem('key', auth.data.key);
-            this.setState({redirect: true})
-        }else{
-            this.setState({
-                redirect: null,
-                username: "",
-                password: ""
-            });
+        mail: this.state.mail,
+      })
+      .then((auth) => {
+        if (auth) {
+          localStorage.setItem("key", auth.data.key);
+          this.setState({ redirect: true });
+        } else {
+          this.setState({
+            redirect: null,
+            username: "",
+            password: "",
+          });
         }
-    }).catch(err => {
+      })
+      .catch((err) => {
         console.log(err);
-    });
-
-  }
+      });
+  };
 
   render() {
     if (this.state.redirect) {
-        return <Redirect to={"/items"} />
-    }else{
-        return (
-            <div className="background">
-              <Link color="white" to="/login" title="Login" >
-                <button>Login</button>
-              </Link>
-              <div className="login">
-                <form onSubmit={this.onSubmit}>
-      
-                  <input type="text" value={this.state.username} placeholder="username" onChange={(e) => this.setState({ username: e.target.value })}></input>
-                  <input type="password" value={this.state.password} placeholder="password" onChange={(e) => this.setState({ password: e.target.value })}></input>
-                  <input type="mail" value={this.state.mail} placeholder="mail" onChange={(e) => this.setState({ mail: e.target.value })}></input>
-                  <input type="submit"></input>
-      
-                </form>
+      return <Redirect to={"/items"} />;
+    } else {
+      return (
+        <div className="background">
+          <Link to="/login" title="Login">
+            Go to Login
+          </Link>
+          <div className="login">
+            <form className="login-form" onSubmit={this.onSubmit}>
+              <h1 class="mb-3">Register</h1>
+              <div class="input-group mb-3">
+                <input
+                  type="text"
+                  value={this.state.username}
+                  placeholder="Username"
+                  onChange={(e) => this.setState({ username: e.target.value })}
+                  class="form-control"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                />
               </div>
-            </div>
-          );
+
+              <div class="input-group mb-3">
+                <input
+                  type="password"
+                  value={this.state.password}
+                  placeholder="Password"
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                  class="form-control"
+                  aria-label="Password"
+                  aria-describedby="basic-addon2"
+                />
+              </div>
+
+              <div class="input-group mb-3">
+                <input
+                  type="mail"
+                  value={this.state.mail}
+                  placeholder="Mail"
+                  onChange={(e) => this.setState({ mail: e.target.value })}
+                  class="form-control"
+                  aria-label="mail"
+                  aria-describedby="basic-addon3"
+                />
+              </div>
+
+              <input class="btn btn-outline-primary" value="Register" type="submit"></input>
+            </form>
+          </div>
+        </div>
+      );
     }
   }
 }
