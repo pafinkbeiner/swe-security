0d1142572807f33cd3bd401251293f6c5119aa3d
Benita <benita.dietrich@web.de>
2020-11-27 15:49:46 +0100

    fixed item page

--- Code/frontend/src/components/Login/Login.jsx
+++ Code/frontend/src/components/Login/Login.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./Login.scss";
-import axios from 'axios';
-import {Redirect, Link} from "react-router-dom";
+import axios from "axios";
+import { Redirect, Link } from "react-router-dom";
 
 export default class Login extends Component {
   constructor(props) {
@@ -22,66 +22,70 @@ export default class Login extends Component {
   onSubmit = (e) => {
     e.preventDefault();
 
-    axios.post('http://localhost:5001/login',{
+    axios
+      .post("http://localhost:5001/login", {
         username: this.state.username,
         password: this.state.password,
-    }).then(auth => {
-
-        if(auth){
-            localStorage.setItem('key', auth.data.key);
-            this.setState({redirect: true})
-        }else{
-            this.setState({
-                redirect: null,
-                username: "",
-                password: ""
-            });
+      })
+      .then((auth) => {
+        if (auth) {
+          localStorage.setItem("key", auth.data.key);
+          this.setState({ redirect: true });
+        } else {
+          this.setState({
+            redirect: null,
+            username: "",
+            password: "",
+          });
         }
-    }).catch(err => {
+      })
+      .catch((err) => {
         console.log(err);
-    });
-  }
+      });
+  };
 
   render() {
-
     if (this.state.redirect) {
-      return <Redirect to={"/items"} />
-  }else{
-    return (
-      <div className="background">
-                <Link color="white" to="/register" title="Register" >
-                <button>Register</button>
-              </Link>
-        <div className="login">
-          <form onSubmit={this.onSubmit}>
-            <FormGroup controlId="username" className="form-row">
-              <FormControl
-                type="text"
-                value={this.state.username}
-                size="lg"
-                placeholder="username"
-                onChange={(e) => this.setState({ username: e.target.value })}
-              />
-            </FormGroup>
-            <FormGroup controlId="password" className="form-row">
-              <FormControl
-                type="password"
-                value={this.state.password}
-                placeholder="Passwort"
-                onChange={(e) => this.setState({ password: e.target.value })}
-              />
-            </FormGroup>
-            <FormGroup controlId="button" className="form-row">
-              <Button bsSize="large" block disabled={this.canLogin} type="submit">
-                Login
-              </Button>
-            </FormGroup>
-          </form>
+      return <Redirect to={"/items"} />;
+    } else {
+      return (
+        <div className="background">
+          <Link color="white" to="/" title="Register">
+            Go to Register
+          </Link>
+
+          <div className="login">
+            <form className="login-form" onSubmit={this.onSubmit}>
+              <h1 class="mb-3">Login</h1>
+              <div class="input-group mb-3">
+                <input
+                  type="text"
+                  value={this.state.username}
+                  placeholder="Username"
+                  onChange={(e) => this.setState({ username: e.target.value })}
+                  class="form-control"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                />
+              </div>
+
+              <div class="input-group mb-3">
+                <input
+                  type="password"
+                  value={this.state.password}
+                  placeholder="Password"
+                  onChange={(e) => this.setState({ password: e.target.value })}
+                  class="form-control"
+                  aria-label="Password"
+                  aria-describedby="basic-addon2"
+                />
+              </div>
+
+              <input class="btn btn-outline-primary" value="Login" type="submit"></input>
+            </form>
+          </div>
         </div>
-      </div>
-    );
-  }
-
-
+      );
+    }
   }
 }
