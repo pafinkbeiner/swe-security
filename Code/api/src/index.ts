c2e936ce014a0c16c3605cee3a67a288ee6557f2
pafinkbeiner <paul-d-f@web.de>
2020-11-30 15:55:32 +0100

    fix

--- Code/api/src/index.ts
+++ Code/api/src/index.ts
@@ -30,7 +30,7 @@ app.use(cors());
 
 // Allow cors
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Origin", "https://swe-shop.paul-finkbeiner.de"); // update to match the domain you will make the request from
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
