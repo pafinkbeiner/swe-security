ce88e5e3b07ae09d751d0b1679e5cbbac306f906
pafinkbeiner <paul-d-f@web.de>
2020-11-30 13:53:15 +0100

    Ready

--- Code/frontend/src/components/Navigation/Navigation.jsx
+++ Code/frontend/src/components/Navigation/Navigation.jsx
@@ -39,7 +39,7 @@ export default class Navigation extends Component {
 
   render() {
     if (this.state.redirect != undefined) {
-      return <Redirect to={"/"} />;
+      return <Redirect to={"/login"} />;
     } else {
       return (
         <nav className="navbar navbar-light bg-light navbar-expand-lg justify-content-between">
@@ -60,7 +60,21 @@ export default class Navigation extends Component {
             }
 
           </ul>
-          <form className="form-inline" onSubmit={this.onSubmit}>
+          {  
+            localStorage.getItem("key") == undefined ? 
+            
+            <form className="form-inline" onSubmit={this.onSubmit}>
+            <Link
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit" to="/register"
+            >
+              Register
+            </Link>
+            </form>
+
+            :
+
+            <form className="form-inline" onSubmit={this.onSubmit}>
             <button
               className="btn btn-outline-success my-2 my-sm-0"
               type="submit"
@@ -68,6 +82,8 @@ export default class Navigation extends Component {
               Logout
             </button>
           </form>
+
+          }
         </nav>
       );
     }
